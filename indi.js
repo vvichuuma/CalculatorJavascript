a8191c1bf8f587be4e89f5f3997255fed868892e
VISHNU <vvichugit@gmail.com>
2019-06-27 13:12:24 -0500

    this is the final commit

--- indi.js
+++ indi.js
@@ -1,1 +1,66 @@
-document.querySelector("#textValue").placeholder = "0";
+document.querySelector("#textValue").placeholder = 0;
+
+var res = 0; 
+
+function add(){
+    
+    var a = (document.querySelector("#textValue").value);
+
+    if(a == ""){
+        
+        document.querySelector("#resCode").innerHTML = `<h3>${0}</h3>`;
+        console.log("zero");
+        
+    }else{
+    res += parseInt(a);
+    
+    document.querySelector("#resCode").innerHTML = `<h3>${res}</h3>`;
+    }
+}
+
+function sub(){
+    
+    var b = (document.querySelector("#textValue").value);
+    
+    if(b === ""){
+        
+    document.querySelector("#resCode").innerHTML = `<h3>${0}</h3>`;
+        console.log("zero");
+        
+    }else{
+    res -= parseInt(b);
+    
+     document.querySelector("#resCode").innerHTML = `<h3>${res}</h3>`;
+    
+    }
+}
+
+function mul(){
+    var c = (document.querySelector("#textValue").value);
+    
+     if(c === ""){
+         
+    document.querySelector("#resCode").innerHTML = `<h3>${0}</h3>`;
+        console.log("zero");
+         
+     }else{
+        res = res *  parseFloat(c);
+    
+     document.querySelector("#resCode").innerHTML = `<h3>${res}</h3>`;
+     }
+}
+
+function div(){
+    var d = (document.querySelector("#textValue").value);
+    
+      if(d === ""){
+          
+    document.querySelector("#resCode").innerHTML = `<h3>${0}</h3>`;
+        console.log("zero");
+          
+      }else{
+        res = res / parseFloat(d);
+    
+     document.querySelector("#resCode").innerHTML = `<h3>${res}</h3>`;
+      }
+}
